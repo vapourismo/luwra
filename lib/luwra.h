da3ec36e045b4c96a4759a7791a009ea16113067
Ole Krüger <ole@vprsm.de>
2015-06-20 15:10:27 +0200

    Add luwra.h which includes all headers collectively

---
+++ lib/luwra.h
@@ -0,0 +1,10 @@
+#ifndef LUWRA_H_
+#define LUWRA_H_
+
+#include "luwra/common.h"
+#include "luwra/types.h"
+#include "luwra/stack.h"
+#include "luwra/userdata.h"
+#include "luwra/wrappers.h"
+
+#endif
