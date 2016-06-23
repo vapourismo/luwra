0be76171154b45fe63fb8f54b6decc39e7d27869
Ole <ole@vprsm.de>
2016-06-23 02:33:28 +0200

    Save profiling example

--- examples/playground.cpp
+++ examples/playground.cpp
@@ -1,26 +1,27 @@
 #include <luwra.hpp>
 #include <iostream>
-#include <string>
-
-using namespace luwra;
-
-struct A {
-	void f() {
-		std::cout << "Hello World" << std::endl;
-	}
-};
-
-struct B: A {};
+#include <list>
 
 int main() {
-	StateWrapper state;
-	state.loadStandardLibrary();
+	luwra::StateWrapper state;
 
-	registerUserType<A()>(state, "A", {LUWRA_MEMBER(A, f)});
-	registerUserType<B()>(state, "B", {LUWRA_MEMBER(B, f)});
+	if (state.runString("foo = function (s) return s .. s; end") != LUA_OK) {
+		std::cerr << luwra::read<std::string>(state, -1) << std::endl;
+		return 1;
+	}
 
-	if (state.runString("B():f()") != LUA_OK)
-		std::cerr << read<std::string>(state, -1) << std::endl;
+	luwra::NativeFunction<std::string> foo = state["foo"];
+	std::string input = "Hello World";
+	std::string expected_output = input + input;
+
+	for (size_t i = 0; i < 50000000; i++) {
+		std::string result = foo(input);
+
+		if (result != expected_output) {
+			std::cerr << "Error" << std::endl;
+			return 1;
+		}
+	}
 
 	return 0;
 }
