253d6735fc2e888672a41b4d33019e4378d2c243
Ole <ole@vprsm.de>
2016-06-30 17:18:10 +0200

    tests: Remove unnecessarily explicit keywords

--- tests/internal/typelist.cpp
+++ tests/internal/typelist.cpp
@@ -8,12 +8,12 @@ TEST_CASE("TypeList") {
 	SECTION("Add") {
 		using Subject = TypeList<>;
 
-		using Result1 = typename Subject::Add<short>;
+		using Result1 = Subject::Add<short>;
 		using Expected1 = TypeList<short>;
 
 		REQUIRE((std::is_same<Result1, Expected1>::value));
 
-		using Result2 = typename Result1::Add<int>;
+		using Result2 = Result1::Add<int>;
 		using Expected2 = TypeList<short, int>;
 
 		REQUIRE((std::is_same<Result2, Expected2>::value));
@@ -22,7 +22,7 @@ TEST_CASE("TypeList") {
 	SECTION("Relay") {
 		using Subject = TypeList<char, short>;
 
-		using Result = typename Subject::template Relay<TypeList>;
+		using Result = Subject::Relay<TypeList>;
 		using Expected = Subject;
 
 		REQUIRE((std::is_same<Result, Expected>::value));
@@ -31,19 +31,19 @@ TEST_CASE("TypeList") {
 	SECTION("Drop") {
 		using Subject1 = TypeList<>;
 
-		using Result1 = typename Subject1::template Drop<1>;
+		using Result1 = Subject1::Drop<1>;
 		using Expected1 = TypeList<>;
 
 		REQUIRE((std::is_same<Result1, Expected1>::value));
 
 		using Subject2 = TypeList<char, short, int>;
 
-		using Result2 = typename Subject2::template Drop<2>;
+		using Result2 = Subject2::Drop<2>;
 		using Expected2 = TypeList<int>;
 
 		REQUIRE((std::is_same<Result2, Expected2>::value));
 
-		using Result3 = typename Result2::template Drop<1>;
+		using Result3 = Result2::Drop<1>;
 		using Expected3 = TypeList<>;
 
 		REQUIRE((std::is_same<Result3, Expected3>::value));
@@ -53,12 +53,12 @@ TEST_CASE("TypeList") {
 		using Subject1 = TypeList<char, short>;
 		using Subject2 = TypeList<int, long>;
 
-		using Result1 = typename Subject1::template Append<Subject2>;
+		using Result1 = Subject1::Append<Subject2>;
 		using Expected1 = TypeList<char, short, int, long>;
 
 		REQUIRE((std::is_same<Result1, Expected1>::value));
 
-		using Result2 = typename Subject2::template Append<Subject1>;
+		using Result2 = Subject2::Append<Subject1>;
 		using Expected2 = TypeList<int, long, char, short>;
 
 		REQUIRE((std::is_same<Result2, Expected2>::value));
--- tests/internal/types.cpp
+++ tests/internal/types.cpp
@@ -8,12 +8,12 @@ TEST_CASE("With") {
 	using Subject = With<char>;
 
 	SECTION("ConstructSignature") {
-		using Result1 = typename Subject::template ConstructSignature<>;
+		using Result1 = Subject::ConstructSignature<>;
 		using Expected1 = char ();
 
 		REQUIRE((std::is_same<Result1, Expected1>::value));
 
-		using Result2 = typename Subject::template ConstructSignature<short, int>;
+		using Result2 = Subject::ConstructSignature<short, int>;
 		using Expected2 = char (short, int);
 
 		REQUIRE((std::is_same<Result2, Expected2>::value));
